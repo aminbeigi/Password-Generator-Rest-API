93ed22907bfa1d56271e13d83a50ca446482167b
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-08 21:38:49 +1100

    Commit before doing crazy stuff

--- passwordgenerator/password_generator.py
+++ passwordgenerator/password_generator.py
@@ -7,8 +7,17 @@ from random import choice
 API_URL = 'https://api.datamuse.com/words?rel_trg='
 BROWSER = mechanicalsoup.Browser()
 WORD_LST = ['cat', 'window', 'dog', 'horse', 'barn']
+WORD_LST_PATH = 'wordlist.txt'
 
 class PasswordGenerator():
+    def get_word(self):
+        with open(WORD_LST_PATH) as f:
+            lines = f.readlines()
+            random_num = randint(0, len(lines))
+            random_word = lines[random_num]
+            random_word = random_word[0:-1] # cull new line character
+        return random_word
+
     def case_randomizer(self, string):
         password = ''
         for char in string:
@@ -26,13 +35,18 @@ class PasswordGenerator():
 
         for i in range(0, password_count):
             for j in range(0, 2):
-                random_word = choice(WORD_LST)
+                random_word = self.get_word()
                 output_word_lst.append(random_word)
 
-                url = API_URL + f'{random_word}' + '&max=5' # limit response length
+                url = API_URL + f'{random_word}&max=5' # limit response length
 
                 response = BROWSER.get(url)
                 data = json.loads(response.text)
+
+                # avoid indexing errors
+                if len(data) == 0:
+                    password_lst.append(random_word)
+
                 random_num = randint(0, 4)
                 password_lst.append(data[random_num]['word'])
 
