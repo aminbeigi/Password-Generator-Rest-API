e547ca47c740e6416485e1fdd4b9191c855aabd6
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-12 10:28:11 +1100

    Add webargs module

--- passwordgenerator/__main__.py
+++ passwordgenerator/__main__.py
@@ -1,23 +1,28 @@
 from flask import Flask, request
-from flask_restful import Api, Resource, abort
+from flask_restful import Api, Resource
 from passwordgenerator.data_generator import DataGenerator
 from passwordgenerator import api, app
+from webargs import fields, validate
+from webargs.flaskparser import use_args, use_kwargs, parser, abort
 
 """A restful API"""
 
 data_generator = DataGenerator()
 
-class DefaultRandomPassword(Resource):
-    def get(self):
+class RandomPassword(Resource):
+    
+    args = {
+        'limit': fields.Int(required=True)
+    }
+
+    @use_args(args, location="query")
+    def get(self, limit):
+        if limit > 20:
+            abort(f"input of {limit} is greater than the maximum 20")
+
+        print(limit)
         default_password_size = 5
         data = data_generator.generate_random(default_password_size)
-        return data
-
-class RandomPassword(Resource):
-    def get(self, data_length_count):
-        if data_length_count > 20:
-            abort(f"input of {data_length_count} is greater than the maximum 20")
-        data = data_generator.generate_random(data_length_count)
         return data
 
 # input is assumed to be seperated by &
@@ -34,7 +39,15 @@ class CustomPassword(Resource):
         data = data_generator.generate_custom(data_length_count, user_input)
         return data
 
-api.add_resource(DefaultRandomPassword, '/password/random')
-api.add_resource(RandomPassword, '/password/random/<int:data_length_count>')
+# This error handler is necessary for usage with Flask-RESTful
+@parser.error_handler
+def handle_request_parsing_error(err, req, schema, *, error_status_code, error_headers):
+    """webargs error handler that uses Flask-RESTful's abort function to return
+    a JSON error response to the client.
+    """
+    abort(error_status_code, errors=err.messages)
+
+api.add_resource(RandomPassword, '/password/random')
+
 api.add_resource(DefaultCustomPassword, '/password/<string:user_input>')
 api.add_resource(CustomPassword, '/password/<string:user_input>/<int:data_length_count>')
