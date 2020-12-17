6536ad4161bfb04652f35bd7f69a86ac03a98d44
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-17 11:14:11 +1100

    Main entry to program in now __init__.py

--- passwordgenerator/__init__.py
+++ passwordgenerator/__init__.py
@@ -4,4 +4,5 @@ from flask_restful import Api
 app = Flask(__name__)
 api = Api(app)
 
-from passwordgenerator import __main__
+from passwordgenerator.__main__ import *
+app.run(debug=True)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='passwordgenerator',
+    version='1.0.0',
+    description='A Rest api.',
+    packages=['passwordgenerator'],
+    license='MIT',
+    author='Amin Ghasembeigi',
+    author_email='me@aminbeigi.com',
+    url='https://github.com/aminbeigi/Password-Generator-Rest-API'
+)
