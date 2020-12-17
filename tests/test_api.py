c17ac25dfa4bbb0376529ab3c5caf42dc5373445
Amin Ghasembeigi <me@aminbeigi.com>
2020-12-17 11:48:24 +1100

    Add more test cases for status code 200

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,17 +1,32 @@
 import unittest
-import passwordgenerator
+from flask import Flask
+from flask_testing import TestCase
 from requests import get, head
 
-PREFIX = 'http://127.0.0.1:5000'
+PREFIX = 'http://127.0.0.1:5000/'
 
-class TestApiResponse(unittest.TestCase):
+class TestApiResponse(TestCase):
+    def create_app(self):
+
+        app = Flask(__name__)
+        app.config['TESTING'] = True
+        return app
+
     def test_200(self):
-        URL = PREFIX + '/api/password/random'
-        r = head(URL) 
+        URL = PREFIX + 'api/password/random'
+        r = head(URL) # e.g. <Response [200]>
         self.assertEqual(r.status_code , 200)
 
-        URL = PREFIX + '/api/password/random'
-        r = head(URL) # e.g. <Response [200]>
+        URL = PREFIX + 'api/password/random?limit=2'
+        r = head(URL)
+        self.assertEqual(r.status_code , 200)
+
+        URL = PREFIX + 'api/password?words=cat&words=computer'
+        r = head(URL)
+        self.assertEqual(r.status_code , 200)
+
+        URL = PREFIX + 'api/password?words=cat&words=computer?limit=2'
+        r = head(URL)
         self.assertEqual(r.status_code , 200)
     
 
